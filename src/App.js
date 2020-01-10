cc8365f077c985ba79167808c70b56ce6ac2f07f
Benhari <harimohan.info@gmail.com>
2020-01-10 10:43:40 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Intermap from './component/interMap/intermap';
 function App() {
   return (
     <div className="App">
-      <h1>Hello, </h1>
+      <h1>Hello, I am Location locator </h1>
       <Location/>
       <Intermap/>
     
